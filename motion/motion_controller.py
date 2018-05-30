9f7f659b710693918de3f040b118ae5701c7324e
Joris Albers <JAL@JAL.local>
2018-05-30 22:38:59 +0200

    Added basic threading functions

--- motion/motion_controller.py
+++ motion/motion_controller.py
@@ -1,3 +1,39 @@
-#
-# Controls the motor speed and direction
-#
+import threading
+import motion_watcher
+from .. import Timing
+
+class motion_controller(threading.Thread):
+    """
+    Controls the motion of a motor
+    """
+    
+    #The maximum rounds per minute allowed
+    MAXIMUM_RPM = 20
+
+    def __init__(self,motion_watcher):
+        """
+        MotionWatcher motion_watcher - class providing speed and direction feedback
+        """
+        self.motion_watcher = motion_watcher
+
+        #Threading
+        threading.Thread.__init__(self)
+        self.stop_event = threading.Event()
+
+    def run(self):
+        """
+        Start the motion controller.
+        """
+        
+
+    def stop(self, stop_motion_watcher = True):
+        """Stop the motion controller"""
+        print("Stopping motion controller")
+        if(stop_motion_watcher):
+            self.motion_watcher.stop()
+        self.stop_event.set()
+
+    def stopped(self):
+        """Check if the motion controller is stopping"""
+        return self.stop_event.is_set()
+    
