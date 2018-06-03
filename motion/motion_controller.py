9914c88a23cf7f002a71845678c69f5049b84a13
Martijn <martijn.van.der.heiden@dpdk.com>
2018-06-03 19:30:32 +0200

    Added add_motion()

--- motion/motion_controller.py
+++ motion/motion_controller.py
@@ -21,6 +21,7 @@ class motion_controller(threading.Thread):
         """
         self.motion_watcher = motion_watcher
 
+        self.fifo = Queue()
         #GPIO
         #   PWM
         self.pinPWM = pinPWM
@@ -40,7 +41,27 @@ class motion_controller(threading.Thread):
         Start the motion controller.
         """
         self.PWM.start(0)
-            
+
+
+    def add_motion(speed,forwards,until):
+        """
+        Adds a new motion to the FIFO\n
+        int speed - the RPM at which to rotate.
+        bool forwards - true to rotate forwards, false to rotate backwards.
+        int until - the UNIX timestamp at which the motion will stop.
+        """
+        if(not isinstance(speed,int)):
+            raise ValueError("The speed must be an integer")
+        if(speed < 0 or speed > self.MAXIMUM_RPM):
+            raise ValueError("The speed must be between 0 and {0}".format(str(self.MAXIMUM_RPM)))
+        if(not isinstance(forwards,bool)):
+            raise ValueError("The forwards parameter must be a bool")
+        if(not isinstance(until,int)):
+            raise ValueError("The until must be an integer, representing an UNIX timestamp")
+        # TODO check if until is a valid timestamp
+        # TODO check if the until timestamp has not already passed
+        self.fifo.put([speed,forwards,until])
+    
     def stop(self, stop_motion_watcher = True):
         """Stop the motion controller"""
         print("Stopping motion controller")
