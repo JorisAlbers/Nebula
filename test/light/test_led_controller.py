fffbbe1878070ae9da9842b60e200908a12a4ef2
Joris Albers <JAL@JAL.local>
2018-06-10 14:31:06 +0200

    Fixed imports

--- test/light/test_led_controller.py
+++ test/light/test_led_controller.py
@@ -2,8 +2,8 @@ import time
 
 def test_sliding_pattern():
     from neopixel import Color
-    from ...light.led_controller import led_controller
-    from ...light.light_animation import SlidingPatterns
+    from ...nebula.light.led_controller import led_controller
+    from ...nebula.light.light_animation import SlidingPatterns
     print("starting sliding patterns test")
     patterns = [[Color(255,0,0),Color(0,255,0),Color(0,255,0)]]
     sp = SlidingPatterns(patterns)
