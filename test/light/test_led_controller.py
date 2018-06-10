e2a7e8c6652a1d63f62baef882f68af0d5a7d546
Joris Albers <JAL@JAL.local>
2018-06-10 18:29:29 +0200

    Updated colors in pattern

--- test/light/test_led_controller.py
+++ test/light/test_led_controller.py
@@ -5,7 +5,7 @@ def test_sliding_pattern(run_for_seconds):
     from ...nebula.light.led_controller import led_controller
     from ...nebula.light.light_animation import SlidingPatterns
     print("starting sliding patterns test")
-    patterns = [[Color(255,0,0),Color(0,255,0),Color(0,255,0)]]
+    patterns = [[Color(255,0,0),Color(0,0,255),Color(0,255,0),Color(0,0,0)]]
     sp = SlidingPatterns(patterns)
     lc = led_controller(18,800000,5,75,75,75,75)
     lc.start()
