629271c77b53edbcd6940ba05ccc34a0bf5c2490
Joris <joris@A-New-MacBook-Pro-5.local>
2018-06-25 16:12:29 +0200

    Added example led_strip to test if we can set the full length of the ledstrip

---
+++ examples/light/ex_led_strip.py
@@ -0,0 +1,21 @@
+def length_test(length):
+    from ...nebula.light.led_strip import NeoPixelLedStrip, Color
+    import time
+    strip = NeoPixelLedStrip(length,18,800000,5,False)
+    strip.begin()
+    pattern = [Color(0,255,0), Color(200,0,0), Color(150,0,0), Color(100,0,0) Color(50,0,0)]
+
+    for x in range(0,len(pattern)):
+        strip.setPixelColor(x,pattern[x])
+        strip.setPixelColor(length-1-x, pattern[len(pattern)-1-x])
+    
+    strip.show()
+
+    time.sleep(5)
+
+    for x in range(0,length):
+        strip.setPixelColor(x,Color(255,0,0))
+        strip.show()
+        time.sleep(0.05)
+    
+
