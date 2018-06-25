7c2a2b7019920ea125cb233cd73807617f5dda72
Joris <joris@A-New-MacBook-Pro-5.local>
2018-06-25 17:22:48 +0200

    Makes the Animation a container class for a light or motion animation and a duration

--- nebula/animation/animation.py
+++ nebula/animation/animation.py
@@ -2,11 +2,19 @@ from ..light.light_animation import LightAnimation
 from ..motion.motion_animation import MotionAnimation
 
 class Animation(object):
-    def __init__(self, lightAnimation, motionAnimation):
-        if not isinstance(lightAnimation, LightAnimation):
-            raise ValueError("The light animation must be an animation!")
-        if not isinstance(motionAnimation, MotionAnimation):
-            raise ValueError("The motion animation must be an animation!")
-        self.lightAnimation = lightAnimation
-        self.motionAnimation = motionAnimation
+    def __init__(self, light_or_motion_animation, duration):
+        """
+        The animation is a container class for a led or motion animation.
+        The duration of the led or motion animation is also set in the animation
+        """
 
+        if not isinstance(light_or_motion_animation, LightAnimation) and not isinstance(light_or_motion_animation, MotionAnimation):
+            raise ValueError("The animation must be an LightAnimation or MotionAnimation!")
+        if not isinstance(int,duration):
+            raise ValueError("The duration must be an int!")
+
+        self.animation = light_or_motion_animation
+        self.duration = duration
+
+
+    
