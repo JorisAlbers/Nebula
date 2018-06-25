9c75675bf2cc99d43321692fb4f43c540be9e84d
Joris <joris@A-New-MacBook-Pro-5.local>
2018-06-25 17:55:11 +0200

    Updated summaries

--- nebula/animation/animation.py
+++ nebula/animation/animation.py
@@ -2,19 +2,20 @@ from ..light.light_animation import LightAnimation
 from ..motion.motion_animation import MotionAnimation
 
 class Animation(object):
-    def __init__(self, light_or_motion_animation, duration):
+    def __init__(self, light_or_motion_animation, duration_ms):
         """
         The animation is a container class for a led or motion animation.
-        The duration of the led or motion animation is also set in the animation
+        light_or_motion_animation - either a LightAnimation or a MotionAnimation
+        int duration_ms - The duration the animation will be displayed for in miliseconds
         """
 
         if not isinstance(light_or_motion_animation, LightAnimation) and not isinstance(light_or_motion_animation, MotionAnimation):
             raise ValueError("The animation must be an LightAnimation or MotionAnimation!")
-        if not isinstance(int,duration):
+        if not isinstance(int,duration_ms):
             raise ValueError("The duration must be an int!")
 
         self.animation = light_or_motion_animation
-        self.duration = duration
+        self.duration = duration_ms
 
 class Animations(list):
     def __getitem__(self,key):
@@ -27,6 +28,11 @@ class Animations(list):
             raise ValueError("Can only add Animations!")
         super(Animations,self).append(item)
     
-    def addAnimation(self,light_or_motion_animation, duration):
-        animation = Animation(light_or_motion_animation,duration)
+    def addAnimation(self,light_or_motion_animation, duration_ms):
+        """
+        Add a new animation
+        light_or_motion_animation - either a LightAnimation or a MotionAnimation
+        int duration_ms - The duration the animation will be displayed for in miliseconds
+        """
+        animation = Animation(light_or_motion_animation,duration_ms)
         super(Animations,self).append(animation)
