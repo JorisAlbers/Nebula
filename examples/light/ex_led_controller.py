e21ab4eac1e5dc8f9c23a352a7a2be036f9398cb
Joris Albers <JAL@JAL.local>
2018-06-26 22:07:46 +0200

    Added loopMode_iterations_test

--- examples/light/ex_led_controller.py
+++ examples/light/ex_led_controller.py
@@ -79,3 +79,33 @@ def loopMode_duration_test():
     print("It should have taken {0} miliseconds".format(run_for_seconds * 1000))
     lc.stop()
     print("end of loopMode_Duration_test test")
+
+def loopMode_iterations_test():
+    from ...nebula.light.led_controller import LedController
+    from ...nebula.light.led_strip import NeoPixelLedStrip, Color
+    from ...nebula.light.led_drawing import SlidingPatterns
+    from ...nebula.light.light_animation import LightAnimation
+    from ...nebula.animation.animation_loop_mode import LoopMode
+    import time
+
+    print("starting loopMode_iterations_test test")
+    patterns = [[Color(0,0,0),Color(0,0,255)]]
+    # Led strip
+    length_strip = 300
+    led_sections = [[0,74],[149,75],[150,224],[299,225]]
+    strip = NeoPixelLedStrip(length_strip, 18,800000,5,False)
+    # Led controller
+    lc = LedController(strip,led_sections)
+    lc.start()
+    iterations = 10
+
+    start = time.time()
+    la = LightAnimation(SlidingPatterns(patterns),500,LoopMode.ITERATIONS, iterations)
+    print("The lightAnimation loop_value at start is {0}".format(la.loop_value))
+    lc.setAnimation(la)
+    while lc.current_animation is not None:
+        time.sleep(0.1)
+    print("Took {0} miliseconds".format(int(round((time.time()-start) * 1000))))
+    print("The lightAnimation loop_value at end is {0}".format(la.loop_value))
+    lc.stop()
+    print("end of loopMode_iterations_test test")
