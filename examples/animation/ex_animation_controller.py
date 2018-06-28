34792580f7536e8edcea648246f6520a7d26b681
Joris Albers <JAL@JAL.local>
2018-06-28 20:31:31 +0200

    BugFix: now correctly start the controller in lightAnimation_callback()

--- examples/animation/ex_animation_controller.py
+++ examples/animation/ex_animation_controller.py
@@ -20,7 +20,7 @@ def lightAnimation_callback():
     led_controller = LedController(NeoPixelLedStrip(300,18,800000,5,False),[[0,299]])
     controller = AnimationController(led_controller,None)
     print("Starting controller")
-    controller.run()
+    controller.start()
     controller.setNextAnimations(animation,time.time() + 1)
     print("Next animation has been set")
     while controller.current_animation is not None:
