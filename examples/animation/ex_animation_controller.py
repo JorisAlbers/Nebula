a08cd26b4284a9bb74b269b543fc5c78c42563b8
Joris Albers <JAL@JAL.local>
2018-06-29 20:24:34 +0200

    Added wait_in_animation example

--- examples/animation/ex_animation_controller.py
+++ examples/animation/ex_animation_controller.py
@@ -28,4 +28,35 @@ def lightAnimation_callback():
     controller.stop()
     print("End of lightAnimation_callback example")
 
+def wait_in_animation(wait_seconds):
+    print("Start of wait_in_animation example")
+    from ...nebula.animation.animation_controller import AnimationController
+    from ...nebula.animation.animation import Animation
+    from ...nebula.light.led_controller import LedController
+    from ...nebula.light.led_strip import NeoPixelLedStrip, Color
+    from ...nebula.light.led_drawing import SlidingPatterns
+    from ...nebula.light.led_drawing import RepeatingPatterns
+    from ...nebula.light.light_animation import LightAnimation
+    from ...nebula.animation.animation_loop_mode import LoopMode
+
+    lightAnimation1 = LightAnimation(SlidingPatterns([[Color(0,0,255),Color(0,0,0)]]),100,LoopMode.ITERATIONS,20)
+    lightAnimation2 = LightAnimation(RepeatingPatterns([[Color(0,255,255),Color(0,0,0)]]),100,LoopMode.ITERATIONS,20)
+    
+    animation = Animation(False)
+    animation.addLightAnimation(lightAnimation1)
+    animation.addLightWait(wait_seconds * 1000)
+    animation.addLightAnimation(lightAnimation2)
+
+    led_controller = LedController(NeoPixelLedStrip(300,18,800000,5,False),[[0,299]])
+    controller = AnimationController(led_controller,None)
+    print("Starting controller")
+    controller.start()
+    controller.setNextAnimations(animation,time.time() + 1)
+    print("Next animation has been set")
+    
+    time.sleep(15 + wait_seconds)
+    controller.stop()
+    print("End of wait_in_animation example")
+
+
 
