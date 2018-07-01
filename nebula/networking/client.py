660f7114da75d824fb555c39da61f6cff732dd3a
Joris Albers <JAL@JAL.local>
2018-07-01 17:45:40 +0200

    Added basiscs for Client

---
+++ nebula/networking/client.py
@@ -0,0 +1,40 @@
+import socket
+import select
+import threading
+ 
+class Client(threading.Thread):
+    def __init__(self,server_ip,server_port):
+        self.server_ip = server_ip
+        self.server_port = server_port
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.settimeout(2)
+        #Threading
+        threading.Thread.__init__(self)
+        self.stop_event = threading.Event()
+
+    def run(self):
+         # connect to remote host
+        try :
+            self.socket.connect((self.server_ip, self.server_port))
+            print("Connected to server")
+        except :
+            print 'Unable to connect'
+            return
+        
+        while not self.stop_event.isset():
+            socket_list = [self.socket]
+         
+            # Get the list sockets which are readable
+            read_sockets, write_sockets, error_sockets = select.select(socket_list , [], [])
+            
+            for sock in read_sockets:
+                #incoming message from remote server
+                if sock == s:
+                    data = sock.recv(4096)
+                    if not data :
+                        print '\nDisconnected from chat server'
+                        #TODO try to reconnect
+                        return
+                    else :
+                        # TODO parse message and call callback
+                        pass
