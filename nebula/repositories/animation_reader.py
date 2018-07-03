93c119a56b47377c4e6aefb84a940fa75d613dba
Joris Albers <JAL@JAL.local>
2018-07-03 22:31:31 +0200

    Added listAnimations()

--- nebula/repositories/animation_reader.py
+++ nebula/repositories/animation_reader.py
@@ -1,6 +1,8 @@
 import os
 
+
 class AnimationReader(object):
+    ANIMATION_EXTENSION = ".nebula_animation"
     def __init__(self,dir_path):
         """
         string dir_path - The location the animations are stored.
@@ -11,4 +13,12 @@ class AnimationReader(object):
             raise IOError("The dir path does not point to an existing folder!")
         self.dir_path = dir_path
 
-    
+    def listAnimations(self):
+        """
+        Get a list of the names of all the animations present in the repository
+        """
+        animations = []
+        for file in os.listdir(self.dir_path):
+            if file.endswith(self.ANIMATION_EXTENSION):
+                animations.append(file.replace(self.ANIMATION_EXTENSION))
+        return animations
