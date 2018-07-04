7c5e58b3497a875c3d408599095ea56600dc5686
Joris Albers <JAL@JAL.local>
2018-07-04 22:38:38 +0200

    Added parsing of animation from animation.json

--- nebula/repositories/animation_reader.py
+++ nebula/repositories/animation_reader.py
@@ -1,5 +1,9 @@
 import os
 import json
+from ..animation.animation import Animation
+from ..animation.animation_loop_mode import LoopMode
+from ..light.light_animation import LightAnimation
+from ..light.led_drawing import *
 
 class AnimationReader(object):
     ANIMATION_EXTENSION = ".nebula.json"
@@ -36,7 +40,7 @@ class AnimationReader(object):
             print("AnimationReader : Failed to read file at {0}".format(path))
         else:
             try:
-                return file.readlines()
+                return file.read()
             except:
                 print("AnimationReader: Failed to read lines, filepath = {0}".format(path))
             finally:
@@ -45,9 +49,10 @@ class AnimationReader(object):
                 except:
                     print("AnimationReader: Failed to close file after reading, filepath = {0}".format(path))
 
-    def loadAnimation(self,name):
+    def loadAnimation(self,name,client_id):
         """
         Load the animation with the name as file prefix (before .extension)
+        Only retrieves the animation of the client_id
         """
         if not isinstance(name,str):
             raise ValueError("The name must be of type string")
@@ -56,10 +61,76 @@ class AnimationReader(object):
         file_content = self.readFile(file_name)
         if file is not None:
             try:
-                return json.loads(file_content)
-            except:
+                j =  json.loads(file_content)
+                j_animation = j["animations_per_ring"][client_id]
+                loops = j_animation["loops"]
+                animation = Animation(loops)
+                for light_animation in j_animation["light_animations"]:
+                    self.addLightAnimationToAnimation(light_animation,animation)
+                return animation
+            except Exception, e:
                 raise IOError("Failed to load animation {0}, format invalid".format(name))
         else:
            raise IOError("Failed to load animation {0}, could not read file".format(name))
         
 
+    def addLightAnimationToAnimation(self,j_lightAnimation,animation):
+        """
+        Add a light animation or WAIT to the list of ligtanimations of the ANIMATION in the params
+        """
+        if "wait" in j_lightAnimation:
+            #Must be a wait
+            animation.addLightWait(j_lightAnimation["wait"])
+            return
+        else:
+            # Standard stuff
+            loop_mode = self.getLoopMode(j_lightAnimation["loop_mode"])
+            loop_value = j_lightAnimation["loop_value"]
+            frame_duration = j_lightAnimation["frame_duration"]
+
+            # Drawer stuff
+            drawer_string = j_lightAnimation["drawer"].lower()
+            if drawer_string == "slidingpatterns":
+                patterns = []
+                for p in j_lightAnimation["patterns"]:
+                    patterns.append(self.stringArray_to_ColorArray(p))
+                drawer = SlidingPatterns(patterns)
+            elif drawer_string == "repeatingpatterns":
+                patterns = []
+                for p in j_lightAnimation["patterns"]:
+                    patterns.append(self.stringArray_to_ColorArray(p))
+                drawer = RepeatingPatterns(patterns)
+            
+            la = LightAnimation(drawer,frame_duration,loop_mode,loop_value)
+            animation.addLightAnimation(la)
+            
+    def getLoopMode(self,loopMode_str):
+        loopMode_str = loopMode_str.lower()
+        if loopMode_str == "duration":
+            return LoopMode.DURATION
+        elif loopMode_str == "iterations":
+            return LoopMode.ITERATIONS
+        elif loopMode_str == "no_loop":
+            return LoopMode.NO_LOOP
+        raise ValueError("can't parse loopmode, unknown loopMode ({0})".format(loopMode_str))
+        
+
+    def stringToColor(self,string):
+        """
+        Convert a rgb string to a color int.
+        The rgb values can be separated by . or , 
+        """
+        split = string.split('.')
+        if len(split) == 1:
+            split = string.split(",")
+        
+        red = int(split[0])
+        green = int(split[1])
+        blue = int(split[2])
+        return Color(red,green,blue)
+
+    def stringArray_to_ColorArray(self,array):
+        pattern =  []
+        for s in array:
+            pattern.append(self.stringToColor(s))
+        return pattern
--- test/repositories/test_animation_reader.py
+++ test/repositories/test_animation_reader.py
@@ -1,5 +1,9 @@
 import os
 from ...nebula.repositories.animation_reader import AnimationReader
+from ...nebula.animation.animation import Animation
+from ...nebula.animation.animation_loop_mode import LoopMode
+from ...nebula.light.led_drawing import LedDrawer, SlidingPatterns, RepeatingPatterns
+from ...nebula.light.light_animation import LightAnimation
 
 def test_readFile_file_returnsLines():
     dirname = os.path.dirname(__file__)
@@ -8,6 +12,47 @@ def test_readFile_file_returnsLines():
     
     reader = AnimationReader(dir_path)
     lines = reader.readFile(file_name)
-    print(lines)
     assert(lines is not None)
     assert(len(lines) > 0)
+
+def test_loadAnimation_file_returnsCorrectAnimation():
+    # EXPECTED VALUES, from file blue_dot.nebula.json
+    ex_initial_start_delay = 1000
+    ex_loops = False,
+    #First light animation
+    ex1_drawer = SlidingPatterns([[]])
+    ex1_patterns = [["255,0,0","0,0,0"]]
+    ex1_loop_mode = LoopMode.DURATION
+    ex1_loop_value = 100000
+    ex1_frame_duration = 200
+    #Second light animation
+    ex2_wait = 1000
+    # type = int
+    #Third light animation
+    ex2_drawer = RepeatingPatterns([[]])
+    ex2_patterns = [["0,255,0","0,0,0","0.0.0","0.0.0"]]
+    ex2_loop_mode = LoopMode.ITERATIONS
+    ex2_loop_value = 15
+    ex2_frame_duration = 100
+
+    dirname = os.path.dirname(__file__)
+    dir_path = os.path.join(dirname,"../../resources")
+    file_name = "blue_dot"
+    client_id = "ring_1"
+    reader = AnimationReader(dir_path)
+    animation = reader.loadAnimation(file_name,client_id)
+
+    #ASSERTS
+    #animation properties
+    assert(ex_loops == animation.loops)
+    #first light animation
+    la = animation.lightAnimations[0]
+    assert(type(ex1_drawer) == type(la.drawer))
+    assert all([a == b for a, b in zip(ex1_patterns[0], la.drawer.patterns[0])])
+    assert(type(ex1_loop_mode) == type(la.loop_mode))
+    assert(ex1_loop_value == la.ex1_loop_value)
+    assert(ex1_frame_duration == la.ex1_frame_duration)
+    #Second light animation
+
+    #Third light animation
+
