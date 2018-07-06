4957fca27f1276c19042cd182cab99c97402e606
Joris <joris@a-new-mbp-5.home>
2018-07-06 12:45:58 +0200

    Added sliding_patterns_margin example

--- examples/light/ex_led_drawing.py
+++ examples/light/ex_led_drawing.py
@@ -31,6 +31,39 @@ def sliding_pattern(run_for_seconds, frame_duration=100):
     lc.stop()
     print("end of sliding patterns test")
 
+def sliding_patterns_margin(run_for_seconds, frame_duration=100):
+    import time
+    from ...nebula.light.led_controller import LedController
+    from ...nebula.light.led_strip import NeoPixelLedStrip, Color
+    from ...nebula.light.led_drawing import SlidingPatterns
+    from ...nebula.light.light_animation import LightAnimation
+    from ...nebula.animation.animation_loop_mode import LoopMode
+
+    print("starting sliding_patterns_margin test")
+    # Light Animation
+    patterns = [[Color(0,0,0),Color(255,0,0)],[Color(0,0,0),Color(0,255,0)]]
+    margin = 50
+    # Led strip
+    length_strip = 300
+    led_sections = [[0,200]]
+    strip = NeoPixelLedStrip(length_strip, 18,800000,5,False)
+    # Led controller
+    lc = LedController(strip,led_sections)
+
+    #Start
+    lc.start()
+    run_for_seconds = 10
+    lc.setAnimation(LightAnimation(SlidingPatterns(patterns,margin),frame_duration, LoopMode.DURATION, run_for_seconds * 1000))
+
+    time.sleep(run_for_seconds + 1)
+
+    patterns = [[Color(0,0,0),Color(0,0,255)],[Color(0,0,0),Color(0,0,255)]]
+    lc.setAnimation(LightAnimation(SlidingPatterns(patterns,margin),frame_duration,LoopMode.DURATION, run_for_seconds * 1000))
+
+    time.sleep(run_for_seconds + 1)
+
+    lc.stop()
+    print("end of sliding_patterns_margin test")
 
 def repeatingPatterns(run_for_seconds, frame_duration=100):
     import time
